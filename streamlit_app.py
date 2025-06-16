6c069365330bf695ddab3e1a1868ba0ba4d4b820
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-06-16 23:20:00 +0800

    Add Processing of CSV files

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.title('🛣️International  Calculator')
 
-st.write('Hello world!')
+st.write('Upload CSV sensor data exported from Physics Toolbox Sensor Suite')
+
+# File upload 
+uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
+
+# Tabulate data
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("Raw Data Preview", df.head())
+
+    st.success("File Uploaded and Processed")
+
+    # Input Formula for Calculation of IRI
+
