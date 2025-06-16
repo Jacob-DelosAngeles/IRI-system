1ba2e2e20fc20ba8a7fe950ce99009de90747aa4
Francis Jacob Delos Angeles <dfrancisjacob65@gmail.com>
2025-06-16 23:41:08 +0800

    Add descriptions for IRI Calculator

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('🛣️International  Calculator')
+st.title('🛣️IRI Calculator')
 
+st.markdown(
+    "<span style = 'font-size: 16px; color:gray;'>"
+    "International Roughness Index (IRI) is a global standard for evaluating road smoothness. "
+    "It measures how much a vehicle bounces at it moves, based on vertical acceleration data."
+    "</span>",
+    unsafe_allow_html=True
+    )
 st.write('Upload CSV sensor data exported from Physics Toolbox Sensor Suite')
 
 # File upload 
